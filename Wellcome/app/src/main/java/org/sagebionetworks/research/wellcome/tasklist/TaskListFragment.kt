721183818a5f47bd7d3ae1016dba7df01cb47766
Mike DePhillips <mike@sdpdigital.com>
2021-02-11 10:37:09 -0800

    Initial project setup

---
+++ Wellcome/app/src/main/java/org/sagebionetworks/research/wellcome/tasklist/TaskListFragment.kt
@@ -0,0 +1,73 @@
+/*
+ * BSD 3-Clause License
+ *
+ * Copyright 2021  Sage Bionetworks. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ * 1.  Redistributions of source code must retain the above copyright notice, this
+ * list of conditions and the following disclaimer.
+ *
+ * 2.  Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * 3.  Neither the name of the copyright holder(s) nor the names of any contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission. No license is granted to the trademarks of
+ * the copyright holders even if such marks are included in this software.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.sagebionetworks.research.wellcome.tasklist
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.app.ActivityCompat.OnRequestPermissionsResultCallback
+import androidx.fragment.app.Fragment
+import dagger.android.support.DaggerFragment
+import org.sagebionetworks.research.sageresearch.dao.room.AppConfigRepository
+import org.sagebionetworks.research.sageresearch.dao.room.ReportRepository
+import org.sagebionetworks.research.wellcome.R
+import org.sagebionetworks.research.wellcome.TaskLauncher
+import org.slf4j.LoggerFactory
+import javax.inject.Inject
+
+/**
+ * A simple [Fragment] subclass that shows a list of the available surveys and tasks for the app
+ */
+class TaskListFragment : DaggerFragment(), OnRequestPermissionsResultCallback {
+    private val LOGGER = LoggerFactory.getLogger(TaskListFragment::class.java)
+
+    @Inject
+    lateinit var taskLauncher: TaskLauncher
+
+    @Inject
+    lateinit var reportRepo: ReportRepository
+
+    @Inject
+    lateinit var appConfigRepo: AppConfigRepository
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_task_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
---
+++ Wellcome/researchstack-sage/src/main/java/org/sagebionetworks/research/arcvalidation/researchstack/framework/step/SagePhoneInstructionStep.java
@@ -0,0 +1,22 @@
+package org.sagebionetworks.research.arcvalidation.researchstack.framework.step;
+
+public class SagePhoneInstructionStep extends SageInstructionStep {
+    /**
+     * Text to go in text view as hint text
+     */
+    public String phoneHint;
+
+    /* Default constructor needed for serialization/deserialization of object */
+    public SagePhoneInstructionStep() {
+        super();
+    }
+
+    public SagePhoneInstructionStep(String identifier, String title, String detailText) {
+        super(identifier, title, detailText);
+    }
+
+    @Override
+    public Class getStepLayoutClass() {
+        return SagePhoneInstructionStepLayout.class;
+    }
+}
